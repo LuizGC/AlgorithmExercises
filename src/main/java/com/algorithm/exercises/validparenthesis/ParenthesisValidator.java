5d292b537d16877ef43815c2c7bda65dad23b8e0
Luiz Costa <luizaugusto3b@gmail.com>
2022-07-27 20:02:21 +0200

    - adding text descriptions about exercises

--- src/main/java/com/algorithm/exercises/validparenthesis/ParenthesisValidator.java
+++ src/main/java/com/algorithm/exercises/validparenthesis/ParenthesisValidator.java
@@ -5,6 +5,13 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.Stack;
 
+/*
+19 Valid Parentheses
+
+Given a string containing just the characters ’(’, ’)’, ’’, ’’, ’[’ and ’]’, determine if the
+input string is valid. The brackets must close in the correct order, "()" and "()[]" are all
+valid but "(]" and "([)]" are not.
+ **/
 public final class ParenthesisValidator {
 
     private static final Map<Character, Character> PARENTHESIS_GROUP = Map
