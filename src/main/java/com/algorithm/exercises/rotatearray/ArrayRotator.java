9ac3cd7531a9b64fc34b6e24e85b1142081dd39d
Luiz Costa <luizaugusto3b@gmail.com>
2022-07-28 18:39:36 +0200

    - adjusting code

--- src/main/java/com/algorithm/exercises/rotatearray/ArrayRotator.java
+++ src/main/java/com/algorithm/exercises/rotatearray/ArrayRotator.java
@@ -2,14 +2,14 @@ package com.algorithm.exercises.rotatearray;
 
 
 /*
- 1 Rotate Array in Java
+1 Rotate Array in Java
 
- You may have been using Java for a while. Do you think a simple Java array question
- can be a challenge? Let’s use the following problem to test.
- Problem: Rotate an array of n elements to the right by k steps. For example, with n
- = 7 and k = 3, the array [1,2,3,4,5,6,7] is rotated to [5,6,7,1,2,3,4].
- How many different ways do you know to solve this problem?
- */
+You may have been using Java for a while. Do you think a simple Java array question
+can be a challenge? Let’s use the following problem to test.
+Problem: Rotate an array of n elements to the right by k steps. For example, with n
+= 7 and k = 3, the array [1,2,3,4,5,6,7] is rotated to [5,6,7,1,2,3,4].
+How many different ways do you know to solve this problem?
+*/
 public class ArrayRotator {
 
     public int[] rotate(int[] array, int k) {
@@ -26,5 +26,4 @@ public class ArrayRotator {
         System.arraycopy(array, 0, rotatedArray, k, array.length-k);
         return rotatedArray;
     }
-
 }
