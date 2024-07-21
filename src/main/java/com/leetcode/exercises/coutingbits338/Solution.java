eaf04eadb31d92d270ab8362f784dfab196bb190
luizcosta <luizaugusto3b@gmail.com>
2024-07-21 12:26:02 +0200

    - adding tests for supporting methods

---
+++ src/main/java/com/leetcode/exercises/coutingbits338/Solution.java
@@ -0,0 +1,68 @@
+package com.leetcode.exercises.coutingbits338;
+
+
+/**
+ * Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n),
+ * ans[i] is the number of 1's in the binary representation of i.
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: n = 2
+ * Output: [0,1,1]
+ * Explanation:
+ * 0 --> 0
+ * 1 --> 1
+ * 2 --> 10
+ * Example 2:
+ * <p>
+ * Input: n = 5
+ * Output: [0,1,1,2,1,2]
+ * Explanation:
+ * 0 --> 0
+ * 1 --> 1
+ * 2 --> 10
+ * 3 --> 11
+ * 4 --> 100
+ * 5 --> 101
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * 0 <= n <= 105
+ */
+class Solution {
+
+    public int[] countBits(int n) {
+        int[] binary = new int[7];
+        int[] bitNumbers = new int[n + 1];
+        for (int i = 1; i < n; i++) {
+            incrementBinary(binary);
+            bitNumbers[i] = countBitsIn(binary);
+        }
+        return bitNumbers;
+    }
+
+    public void incrementBinary(int[] binary) {
+        int incNexBit = 1;
+        for (int i = 0; i < binary.length; i++) {
+            int bit = binary[i];
+            bit = bit + incNexBit;
+            if (bit == 2) {
+                bit = 0;
+                incNexBit = 1;
+            } else {
+                incNexBit = 0;
+            }
+            binary[i] = bit;
+        }
+    }
+
+    public int countBitsIn(int[] binary) {
+        int numberBits = 0;
+        for (var bit : binary) {
+            numberBits += bit;
+        }
+        return numberBits;
+    }
+}
---
+++ src/test/java/com/leetcode/exercises/coutingbits338/SolutionTest.java
@@ -0,0 +1,57 @@
+package com.leetcode.exercises.coutingbits338;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionTest {
+
+    private static Stream<Arguments> inputProvider() {
+        return Stream.of(
+                Arguments.of(2, new int[]{0, 1, 1})
+        );
+    }
+
+    private static int summingBit(int[] binary) {
+        return Arrays.stream(binary)
+                .sum();
+    }
+
+    @ParameterizedTest
+    @MethodSource("inputProvider")
+    void testCountBits(int n, int[] expected) {
+        var countBits = new Solution();
+
+        var result = countBits.countBits(n);
+
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    void testIncrement() {
+        var countBits = new Solution();
+        int[] binary = new int[7];
+        for (int i = 1; i <= 105; i++) {
+            countBits.incrementBinary(binary);
+            assertEquals(Integer.bitCount(i), summingBit(binary));
+        }
+    }
+
+    @Test
+    void testCountBitsInArray() {
+        var countBits = new Solution();
+        int[] binary = new int[7];
+        for (int i = 1; i <= 105; i++) {
+            countBits.incrementBinary(binary);
+            assertEquals(Integer.bitCount(i), countBits.countBitsIn(binary));
+        }
+    }
+
+}
